e672f28087f292d4814f006f5dd2c332526e5fb6
AnasAhmedFND <anasahmed4402@gmail.com>
2024-10-10 11:12:34 +0600

    billing details

---
+++ src/ecommerce.js
@@ -0,0 +1,20 @@
+
+
+
+let headBillid = document.getElementById("billing");
+headBillid.style.fontSize = "30px"
+let hx = document.getElementById("country");
+hx.style.height = "50px"
+
+let hy = document.getElementById("headCountry");
+hy.style.marginTop = "15px"
+let rx = document.getElementById("input1");
+rx.style.marginTop = "10px"
+let ph = document.getElementById("prod");
+ph.style.paddingBottom = "10px"
+let cxtr = document.getElementById("calar");
+cxtr.style.color = "E1DED9"
+
+let bord = document.getElementById("bor");
+bord.style.borderBlock = "12px"
+
