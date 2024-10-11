91c90e48eb604f0f62fb0ff5ee9e3d89256ce571
AnasAhmedFND <anasahmed4402@gmail.com>
2024-10-11 19:10:50 +0600

    millennial design

---
+++ src/pagees.js
@@ -0,0 +1,54 @@
+// main-section
+let blogOne = document.getElementById("mainsec");
+
+blogOne.style.border = "1px solid black"
+blogOne.style.marginTop = "340px"
+blogOne.style.display = "flex"
+blogOne.style.gap = "60px"
+
+
+// left-side start
+let blogLeft = document.getElementById("left");
+blogLeft.style.border = "1px solid red"
+blogLeft.style.width = "600px"
+blogLeft.style.marginLeft = "100px"
+
+
+// nav
+let naver = document.getElementById("nav");
+naver.style.display = "flex"
+naver.style.gap = "10px"
+naver.style.marginTop = "20px"
+naver.style.alignItems = "center"
+
+// nav/man
+let icon = document.getElementById("man");
+icon.style.marginLeft = "25px"
+
+let xtr = document.getElementById("icon");
+xtr.style.marginLeft = "25px"
+
+// h1
+let goinge = document.getElementById("going");
+goinge.style.fontSize = "25px"
+goinge.style.marginTop = "20px"
+goinge.style.fontWeight = "bold"
+
+// lpp
+let peragrap = document.getElementById("pera");
+peragrap.style.marginTop = "15px"
+peragrap.style.fontSize = "12px"
+peragrap.style.fontStyle = "justify"
+
+// button
+let button = document.getElementById("butt");
+button.style.marginTop = "30px"
+button.style.borderBottom = "2px solid black"
+button.style.paddingBottom = "5px"
+
+
+
+// right-side start
+let bolgRight = document.getElementById("right");
+bolgRight.style.border = "1px solid red"
+bolgRight.style.width = "350px"
