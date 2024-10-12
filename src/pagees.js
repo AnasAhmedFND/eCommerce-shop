d00ea7efcd556758688991bb07fbec99a2f4e3ef
AnasAhmedFND <anasahmed4402@gmail.com>
2024-10-12 11:40:49 +0600

    bolg futtar

--- src/pagees.js
+++ src/pagees.js
@@ -1,15 +1,16 @@
 // main-section
 let blogOne = document.getElementById("mainsec");
 
-blogOne.style.border = "1px solid black"
+
 blogOne.style.marginTop = "340px"
 blogOne.style.display = "flex"
 blogOne.style.gap = "60px"
+blogOne.style.paddingBottom = "30px"
 
 
 // left-side start
 let blogLeft = document.getElementById("left");
-blogLeft.style.border = "1px solid red"
+
 blogLeft.style.width = "600px"
 blogLeft.style.marginLeft = "100px"
 
@@ -20,6 +21,8 @@ naver.style.display = "flex"
 naver.style.gap = "10px"
 naver.style.marginTop = "20px"
 naver.style.alignItems = "center"
+
+
 
 // nav/man
 let icon = document.getElementById("man");
@@ -47,8 +50,135 @@ button.style.borderBottom = "2px solid black"
 button.style.paddingBottom = "5px"
 
 
+//  card-2 start
+let exit = document.getElementById("x");
+exit.style.marginTop = "40px"
+exit.style.position = "relative top 20px"
+
+
+//  card-2 nav
+let max = document.getElementById("navtwo");
+
+max.style.display = "flex"
+max.style.gap = "10px"
+max.style.marginTop = "20px"
+max.style.alignItems = "center "
+
+
+let arrayBuss = document.getElementById("buss");
+arrayBuss.style.marginLeft = "25px"
+
+let bussTwo = document.getElementById("buss2");
+bussTwo.style.marginLeft = "25px"
+
+// c2-h1
+let explorings = document.getElementById("exploring");
+explorings.style.fontSize = "25px"
+explorings.style.marginTop = "20px"
+explorings.style.fontWeight = "bold"
+
+// c2-p
+let peragraps = document.getElementById("pera2");
+peragraps.style.marginTop = "15px"
+peragraps.style.fontSize = "12px"
+peragraps.style.fontStyle = "justify"
+
+// button-c-2
+let buttond = document.getElementById("butt2");
+buttond.style.marginTop = "30px"
+buttond.style.borderBottom = "2px solid black"
+buttond.style.paddingBottom = "5px"
+
+// card3 start
+let cardHead = document.getElementById("cards3");
+cardHead.style.marginTop = "40px"
+
+// nav-3
+let maxes = document.getElementById("iconnav");
+maxes.style.display = "flex"
+maxes.style.gap = "10px"
+maxes.style.marginTop = "20px"
+maxes.style.alignItems = "center "
+
+let icons = document.getElementById("imges");
+icons.style.marginLeft = "25px"
+
+let tests = document.getElementById("imges2");
+tests.style.marginLeft = "25px"
+
+// c--3-h1
+let hendmades = document.getElementById("handmade");
+hendmades.style.fontSize = "25px"
+hendmades.style.marginTop = "20px"
+hendmades.style.fontWeight = "bold"
+
+// c3-pp
+let peratr = document.getElementById("perathree");
+peratr.style.marginTop = "15px"
+peratr.style.fontSize = "12px"
+peratr.style.fontStyle = "justify"
+
+// c3-button
+let buttondThree = document.getElementById("butt3");
+buttondThree.style.marginTop = "30px"
+buttondThree.style.borderBottom = "2px solid black"
+buttondThree.style.paddingBottom = "5px"
+
+
+
+
+
 
 // right-side start
 let bolgRight = document.getElementById("right");
-bolgRight.style.border = "1px solid red"
+
 bolgRight.style.width = "350px"
+
+let sumsed = document.getElementById("serch");
+sumsed.style.border = "1px solid black"
+sumsed.style.width = "full"
+sumsed.style.height = "40px"
+sumsed.style.borderRadius = "5px"
+
+
+let relatives = document.getElementById("relat");
+relatives.style.paddingLeft = "320px"
+relatives.style.paddingTop = "6px"
+
+
+// categories
+let categories =document.getElementById("categories");
+categories.style.marginLeft = "40px"
+categories.style.marginTop = "40px"
+
+
+// hh
+let categors = document.getElementById("categ-h");
+categors.style.fontSize = "25px"
+categors.style.fontWeight = "bold"
+
+let craftss = document.getElementById("crafts");
+craftss.style.marginTop = "25px"
+
+let designs = document.getElementById("design");
+designs.style.marginTop = "25px"
+
+let handmad = document.getElementById("newer");
+handmad.style.marginTop = "25px"
+
+let interio = document.getElementById("Interiors");
+interio.style.marginTop = "25px"
+
+let woodes = document.getElementById("wood");
+woodes.style.marginTop = "25px"
+
+// recent
+let recented = document.getElementById("recentmain");
+recented.style.marginTop = "80px"
+recented.style.marginLeft = "40px"
+
+
+let postses = document.getElementById("posts");
+postses.style.fontWeight = "bold"
+postses.style.fontSize = "20px"
+
